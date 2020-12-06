5b1811043e74c6d55ad2e7d8d1ad36110407a325
Bowl-of-noodles <72003879+Bowl-of-noodles@users.noreply.github.com>
2020-12-07 00:27:33 +0300

    Update ChanceToAddEffect.java

--- src/edu/itmo/rogachova/Moves/ChanceToAddEffect.java
+++ src/edu/itmo/rogachova/Moves/ChanceToAddEffect.java
@@ -2,9 +2,9 @@ package edu.itmo.rogachova.Moves;
 
 public class ChanceToAddEffect
 {
-    public static boolean chance(int perc)
+    public static boolean chance(double perc)
     {
-        return Math.random() <= perc/100? true: false;
+        return Math.random() <= perc? true: false;
 
     }
 }
